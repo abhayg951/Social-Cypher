c01b65d34f756f6adfc98fda60c0e00e6f4cceff
abhayg951 <insuabhay951@gmail.com>
2024-01-04 23:26:22 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,18 @@ from .config import settings
 
 print(settings.database_name)
 
-app = FastAPI()
+app = FastAPI(
+    title="Post System",
+    summary="This API enables users to Create, Read, Update, and Delete records seamlessly while ensuring security through robust user authentication, offering a comprehensive data management solution.",
+    description='''This comprehensive API empowers users with full CRUD capabilities for managing posts while ensuring secure access through user authentication\n. 
+    Beyond basic operations, it features a robust like system, enabling users to interact with posts by viewing and contributing to like counts\n. 
+    With seamless Create, Read, Update, and Delete functionalities, coupled with user authentication and a dynamic like system, this API offers a versatile and secure platform for content management and user engagement, making it ideal for applications where posts play a central role and user interactions are key.''',
+    version="1.0",
+    contact={
+        "name": "Abhay Gupta",
+        "email": "insuabhay951@gmail.com"
+    }
+)
 
 models.Base.metadata.create_all(bind=engine)
 
